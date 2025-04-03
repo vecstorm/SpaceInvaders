4483b3f030abd6ee622f542874e04bfb0bb452f1
INFO2-253901133\Argo <a24vicmilsal@inspedralbes.cat>
2025-04-03 20:37:48 +0200

    colocamos las naves en el canvas

---
+++ JavaScript/js.js
@@ -0,0 +1,39 @@
+var navePlayer1, navePlayer2;
+
+function startGame() {
+    myGameArea.start();
+    navePlayer1 = new component(30, 30, "pink", 1000, 750);
+    navePlayer2 = new component(30, 30, "green", 600, 750);
+}
+// Creacion del canvas
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 1600;
+        this.canvas.height = 870;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+
+}
+// Spawn de naves
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.x = x;
+    this.y = y;
+    
+    this.update = function () {
+        context = myGameArea.context;
+        context.fillStyle = color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+
+function updateGameArea() {
+    myGameArea.clear();
+    
+}
