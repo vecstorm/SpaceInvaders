5a629c3dc295002a90e2ec1e4a343d2b314adcfe
INFO2-253901133\Argo <a24vicmilsal@inspedralbes.cat>
2025-04-03 20:47:18 +0200

    Añadimos el movimiento a una nave

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,6 +1,5 @@
 var navePlayer1, navePlayer2;
 
-
 function startGame() {
     myGameArea.start();
     navePlayer1 = new component(30, 30, "pink", 1000, 750);
@@ -16,9 +15,22 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
 
-        }
-    }
 
+
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = (e.type == "keydown")
+
+        })
+
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = (e.type == "keydown")
+        })
+    },
+    clear: function () { 
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
 // Spawn de naves
 function component(width, height, color, x, y) {
     this.width = width;
@@ -32,10 +44,25 @@ function component(width, height, color, x, y) {
         context.fillStyle = color;
         context.fillRect(this.x, this.y, this.width, this.height);
     }
+
+    this.newPos = function () {
+        this.x += this.speedX;
+
+        // Limitar los bordes
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.width > myGameArea.canvas.width) this.x = myGameArea.canvas.width - this.width;
+    }
 }
 
 
 function updateGameArea() {
     myGameArea.clear();
     
+    // Movimiento nave 1 (A y D)
+    navePlayer1.speedX = 0;
+    if (myGameArea.keys && myGameArea.keys[65]){navePlayer1.speedX = -7;} // A
+    if (myGameArea.keys && myGameArea.keys[68]){navePlayer1.speedX = 7;}  // D
+    navePlayer1.newPos();
+    navePlayer1.update();
+
 }
